89451dec1d948ebb3564c8cb41e77b1bc7fcd0f3
Jörg Kiesewetter <joerg.kiesewetter@gmail.com>
2023-11-18 02:25:51 +0300

    added guardFactory

---
+++ transaction-guard/contracts/GuardFactory.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.8.0 <0.9.0;
+
+import "./TransactionGuard.sol";
+
+contract GuardFactory {
+    mapping(address => address) private guards;
+
+    function createGuard(
+        address safeAddress, 
+        string memory name
+    ) public {
+        RestrictedTransactionGuard guard = new RestrictedTransactionGuard();
+        guards.push(guard);
+    }
+
+    function getGuard(address safeAddress) public view returns (address) {
+        return guards[safeAddress];
+    }
+}
