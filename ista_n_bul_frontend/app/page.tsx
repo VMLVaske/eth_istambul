47bfaaa40786e2214cea360a0784bc23a6970f0a
Jörg Kiesewetter <joerg.kiesewetter@gmail.com>
2023-11-18 13:42:30 +0300

    added some more style stuff

--- ista_n_bul_frontend/app/page.tsx
+++ ista_n_bul_frontend/app/page.tsx
@@ -12,9 +12,93 @@ import { CreateUserRoleModal } from "@/components/createUserRoleModal";
 import { AddContractModal } from "@/components/addContractModal";
 import { AddOwnerModal } from "@/components/addOwnerModal";
 import { useSafeAppsSDK } from "@safe-global/safe-apps-react-sdk";
+import { ethers, BrowserProvider } from "ethers";
+import React, { useEffect, useState } from "react";
+import safeAbi from "../abis/gnosisSafe.json";
+import SetupPage from "@/app/setup/page";
+
+function noGuard() {
+  return (
+    <div>
+      <div>
+        This app helps you setting multiple user permissions for the different
+        owners of your SAFE. This uses SAFE's guard feature to restrict specific
+        users and give them a whitelist of things they can do on-chain.
+      </div>
+      <div>
+        Before we can start, we need to setup a{" "}
+        <a
+          href="https://help.safe.global/en/articles/40809-what-is-a-transaction-guard"
+          target="_blank"
+        >
+          transaction guard
+        </a>{" "}
+        to your safe. Please push the next button to do this.
+      </div>
+      <div>
+        <Button title="Install Transaction Guard" />
+      </div>
+    </div>
+  );
+}
+
+function GuardAvailable(props: {
+  provider: BrowserProvider;
+  guard: string;
+  userRoles: string[];
+}) {
+  return (
+    <div>
+      <h2>User Roles</h2>
+      {props.userRoles.map((userRole) => (
+        <li>{userRole}</li>
+      ))}
+    </div>
+  );
+}
 
 export default function Home() {
   const { sdk, connected, safe } = useSafeAppsSDK();
+  const [provider, setProvider] = useState<BrowserProvider | null>(null);
+  const [guardAddress, setGuardAddress] = useState("");
+  const [userRoles, setUserRoles] = useState<string[]>([]);
+
+  useEffect(() => {
+    const initializeProvider = async () => {
+      if (window.ethereum) {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        const provider = new BrowserProvider(window.ethereum);
+        setProvider(provider);
+
+        // check if guard is installed
+        const safeContract = new ethers.Contract(
+          safe.safeAddress,
+          safeAbi,
+          provider
+        );
+
+        const guard = await safeContract
+          .getStorageAt(
+            "0x4a204f620c8c5ccdca3fd54d003badd85ba500436a431f0cbda4f558c93c34c8",
+            1
+          )
+          .catch(() => "");
+        setGuardAddress(guard);
+
+        if (guard != "") {
+          const guardContract = new ethers.Contract(
+            guardAddress,
+            safeAbi,
+            provider
+          );
+        }
+        // TODO
+        setUserRoles(["test role"]);
+      }
+    };
+
+    initializeProvider();
+  }, [safe, guardAddress]);
 
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
@@ -26,6 +110,17 @@ export default function Home() {
         <div>safe address: {safe.safeAddress}</div>
       </div>
       <div>
+        {(() => {
+          if (guardAddress != "") {
+            return GuardAvailable({
+              provider: provider!,
+              guard: guardAddress,
+              userRoles,
+            });
+          } else {
+            return SetupPage();
+          }
+        })()}
         <CreateUserRoleModal />
         <AddContractModal />
         <AddOwnerModal />
--- ista_n_bul_frontend/app/setup/page.tsx
+++ ista_n_bul_frontend/app/setup/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { Button, Divider } from "@nextui-org/react";
 import react, { useState } from "react";
@@ -6,34 +6,44 @@ import { useRouter } from "next/navigation";
 import { title, subtitle } from "@/components/primitives";
 
 function SetupPage() {
+  const [address, setAddress] = useState("");
+  const [isValid, setIsValid] = useState(true);
+  const router = useRouter();
 
-    const [address, setAddress] = useState('');
-    const [isValid, setIsValid] = useState(true);
-    const router = useRouter();
+  const handleChange = (e: any) => {
+    const input = e.target.value;
+    setAddress(input);
+  };
 
-    const handleChange = (e: any) => {
-        const input = e.target.value;
-        setAddress(input);
-    };
-
-    return (
-        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-            <h1 className={title()}>Deploy</h1>
-            <h2 className={subtitle({ class: "mt-4" })}>Some explanatory text</h2>
-            <div>
-				<Button>Deploy</Button>
-			</div>
-
-            <Divider />
-
-            <h1 className={title()}>Setup</h1>
-            <h2 className={subtitle({ class: "mt-4" })}>Some more explanatory text</h2>
-            <div>
-				<Button>Setup</Button>
-			</div>
-        </section>
-
-    );
+  return (
+    <div>
+      <div>
+        This app helps you setting multiple user permissions for the different
+        owners of your SAFE. This uses SAFE's guard feature to restrict specific
+        users and give them a whitelist of things they can do on-chain.
+      </div>
+      <div>
+        Before we can start, we need to setup a{" "}
+        <a
+          href="https://help.safe.global/en/articles/40809-what-is-a-transaction-guard"
+          target="_blank"
+        >
+          transaction guard
+        </a>{" "}
+        to your safe. This happens in two steps. First, we need to deploy your
+        very own transaction guard. In the second step, we need to add the new
+        guard to your safe.
+      </div>
+      <div>
+        <span>Step 1: </span>
+        <Button>Deploy Transaction Guard</Button>
+      </div>
+      <div>
+        <span>Step 2: </span>
+        <Button>Apply Guard to your SAFE</Button>
+      </div>
+    </div>
+  );
 }
 
 export default SetupPage;
