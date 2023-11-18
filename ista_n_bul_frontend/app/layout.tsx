a6fa82434acddac1733389d86a2fb25a4121eca8
Jörg Kiesewetter <joerg.kiesewetter@gmail.com>
2023-11-18 13:17:30 +0300

    added safe SDK

--- ista_n_bul_frontend/app/layout.tsx
+++ ista_n_bul_frontend/app/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import "@/styles/globals.css";
 import { Metadata } from "next";
 import { siteConfig } from "@/config/site";
@@ -7,22 +9,24 @@ import { Navbar } from "@/components/navbar";
 import { Link } from "@nextui-org/link";
 import clsx from "clsx";
 
-export const metadata: Metadata = {
-	title: {
-		default: siteConfig.name,
-		template: `%s - ${siteConfig.name}`,
-	},
-	description: siteConfig.description,
-	themeColor: [
-		{ media: "(prefers-color-scheme: light)", color: "white" },
-		{ media: "(prefers-color-scheme: dark)", color: "black" },
-	],
-	icons: {
-		icon: "/favicon.ico",
-		shortcut: "/favicon-16x16.png",
-		apple: "/apple-touch-icon.png",
-	},
-};
+import SafeProvider from "@safe-global/safe-apps-react-sdk";
+
+// export const metadata: Metadata = {
+//   title: {
+//     default: siteConfig.name,
+//     template: `%s - ${siteConfig.name}`,
+//   },
+//   description: siteConfig.description,
+//   themeColor: [
+//     { media: "(prefers-color-scheme: light)", color: "white" },
+//     { media: "(prefers-color-scheme: dark)", color: "black" },
+//   ],
+//   icons: {
+//     icon: "/favicon.ico",
+//     shortcut: "/favicon-16x16.png",
+//     apple: "/apple-touch-icon.png",
+//   },
+// };
 
 export default function RootLayout({
 	children,
--- ista_n_bul_frontend/app/page.tsx
+++ ista_n_bul_frontend/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import NextLink from "next/link";
 import { Link } from "@nextui-org/link";
 
@@ -9,23 +11,25 @@ import { Button } from "@nextui-org/react";
 import { CreateUserRoleModal } from "@/components/createUserRoleModal";
 import { AddContractModal } from "@/components/addContractModal";
 import { AddOwnerModal } from "@/components/addOwnerModal";
+import { useSafeAppsSDK } from "@safe-global/safe-apps-react-sdk";
 
 export default function Home() {
-	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>An &nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>EthIstanbul&nbsp;</h1>
-				<h1 className={title()}>Project</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					by jörg and valerie
-				</h2>
-			</div>
-			<div>
-				<CreateUserRoleModal />
-				<AddContractModal />
-				<AddOwnerModal />
-			</div>
-		</section>
-	);
+  const { sdk, connected, safe } = useSafeAppsSDK();
+
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-lg text-center justify-center">
+        <h1 className={title()}>An &nbsp;</h1>
+        <h1 className={title({ color: "violet" })}>EthIstanbul&nbsp;</h1>
+        <h1 className={title()}>Project</h1>
+        <h2 className={subtitle({ class: "mt-4" })}>by jörg and valerie</h2>
+        <div>safe address: {safe.safeAddress}</div>
+      </div>
+      <div>
+        <CreateUserRoleModal />
+        <AddContractModal />
+        <AddOwnerModal />
+      </div>
+    </section>
+  );
 }
